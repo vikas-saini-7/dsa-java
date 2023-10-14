fd25adcc4c00bfd135a6e580f541e0d65b3cee7d
vikas-saini-7 <vikas289888@gmail.com>
2023-10-14 23:05:32 +0530

    Change in rec

--- Recursion5.java
+++ Recursion5.java
@@ -1,3 +1,14 @@
 public class Recursion5 {
     // Recursion day 5 
+    public static void main(String[] args) {
+        int result = sum(10);
+        System.out.println(result);
+      }
+      public static int sum(int k) {
+        if (k > 0) {
+          return k + sum(k - 1);
+        } else {
+          return 0;
+        }
+      }
 }
